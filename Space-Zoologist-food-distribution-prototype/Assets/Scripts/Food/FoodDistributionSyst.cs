0407af1608f286e5b9ebc5a78bd6f3dbdd512fab
Ethan Chiang <ethanyichiang@gmail.com>
2020-01-24 10:58:17 -0800

    init FoodDistributionSystem script

---
+++ Space-Zoologist-food-distribution-prototype/Assets/Scripts/Food/FoodDistributionSyst.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodDistributionSyst : MonoBehaviour
+{
+
+    [SerializeField] private FoodSourceTileMapScript foodSourceTileMapScript;
+    private List<FoodSource> foodSources;
+
+    void getAllFoodSource()
+    {
+        // Get the list of food sources in the reserves
+        foodSources = foodSourceTileMapScript.getFoodSources();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getAllFoodSource();
+    }
+}
