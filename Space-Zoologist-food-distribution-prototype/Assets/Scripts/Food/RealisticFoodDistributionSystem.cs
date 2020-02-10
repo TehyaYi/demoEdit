8d5a593f0b975ce26988a215422ecc20785daab8
Ethan Chiang <ethanyichiang@gmail.com>
2020-02-10 13:32:12 -0800

    update realistic method

--- Space-Zoologist-food-distribution-prototype/Assets/Scripts/Food/RealisticFoodDistributionSystem.cs
+++ Space-Zoologist-food-distribution-prototype/Assets/Scripts/Food/RealisticFoodDistributionSystem.cs
@@ -10,37 +10,77 @@ using UnityEngine;
 
 public class RealisticFoodDistributionSystem : MonoBehaviour
 {
-    // 
-    List<FoodSource> getFoodSourceByType()
+    // Get all the food source on map with the type given
+    private List<FoodSource> getFoodSourceByType(NeedType foodSourceType)
     {
+        List<FoodSource> foodSources = new List<FoodSource>();
 
+        // TODO: get all the food source on map by its type, `foodSourceType`.
+
+        return foodSources;
     }
 
-    List<AnimalPopulation> getPopulationsCanAccess(FoodSource);
-    Bool animalCanConsume(AnimalPopulation, FoodSource.type);
-    List<AnimalPopulation> getPopulationIsEdible(List<AnimalPopulation>, FoodSource);
-    float getCompetionRating(List<AnimalPopulation>);
-    void distributeFoodSource(FoodSource)
-
-
-    void updateFoodType(FoodSource.type)
+    // Get a list of animal population that has access to given food source
+    private List<AnimalPopulation> getPopulationsCanAccess(FoodSource foodSource)
     {
-        List<FoodSource> foodSources = getFoodSourceByType();
+        List<AnimalPopulation> populationsCanAccess = new List<AnimalPopulation>();
+
+        // TODO: get list of animal population that has access to foodSource
+
+        return populationsCanAccess;
+    }
+
+    // Get list of populations that can consume given food source in given list of population
+    private List<AnimalPopulation> getPopulationsThatConsumeFoodSource(List<AnimalPopulation> allPopulations, NeedType foodSourceType)
+    {
+        List<AnimalPopulation> canConsumePopulations = new List<AnimalPopulation>();
+
+        foreach(AnimalPopulation population in allPopulations)
+        {
+            // TODO: check in population can consume foodSourceType
+            if (true)
+            {
+                canConsumePopulations.Add(population);
+            }
+        }
+
+        return canConsumePopulations;
+    }
+
+    private float getCompetionRating(List<AnimalPopulation> populations)
+    {
+        float competitionRating = 0;
+
+        // TODO: compute competition rating(standard dev) of the donimance rating for the populations
+
+        return competitionRating;
+    }
+
+    private void distributeFoodSource(FoodSource foodSource)
+    {
+        // TODO: distribute food source (same as naive method)
+    }
+
+
+    // This function will be envoked when a food source is marked "dirty"
+    public void updateFoodType(NeedType foodSourceType)
+    {
+        List<FoodSource> foodSources = getFoodSourceByType(foodSourceType);
 
         SortedList ratingAndPopulationPair = new SortedList();
 
-        forEach(fs in foodSources)
+        foreach(FoodSource foodSource in foodSources)
         {
-            List<AnimalPopulation> animalPopulations = getPopulationsCanAccess(fs);
-            List<AnimalPopulation> populationsThatCanConsumeFoodSource(animalPopulations, fs);
+            List<AnimalPopulation> animalPopulations = getPopulationsCanAccess(foodSource);
+            List<AnimalPopulation> populationsThatCanConsumeFoodSource = getPopulationsThatConsumeFoodSource(animalPopulations, foodSource.Type);
             float competionRating = getCompetionRating(populationsThatCanConsumeFoodSource);
 
-            ratingAndPopulationPair.Add(competionRating, fs);
+            ratingAndPopulationPair.Add(competionRating, foodSource);
         }
 
         foreach (DictionaryEntry pair in ratingAndPopulationPair)
         {
-            distributeFoodSource(pair.Value);
+            distributeFoodSource((FoodSource)pair.Value);
         }
     }
 }
